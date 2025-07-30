63b6611983907d62431dfada6ca2d5f426707fdd
HayrunisaPolat <nisapl20t@gmail.com>
2025-07-30 16:09:34 +0300

    Update swagger-initializer.js

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "./auth_api8.json",
+    url: "./auth_api.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
